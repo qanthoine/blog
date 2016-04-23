bdbb452bf145a19d6297a4c1b375988f97cfa57d
qanthoine <qanthoine@gmail.com>
2016-04-23 15:26:22 +0200

    Ajout d'une indication

--- admin/includes/ad_header.php
+++ admin/includes/ad_header.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html>
 <header>
-	<center><img src="../img/header.jpg"/></center>
+	<center>
+		<img src="../img/header.jpg"/>
+		<h3>Espace Administration</h3>
+	</center>
 </header>
 </html>
